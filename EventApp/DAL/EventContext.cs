4f3aa277c9e58ce023407ec472701054e0f8b01c
Brandon Ferguson <syntaxic@gmail.com>
2018-06-30 14:52:25 -0400

    Added EventContext Class

---
+++ EventApp/DAL/EventContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EventApp.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EventApp.DAL
+{
+    public class EventContext : DbContext
+    {
+        public EventContext() : base("EventContext")
+        {
+
+        }
+
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
