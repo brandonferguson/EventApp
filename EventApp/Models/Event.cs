43350376bb0a7d396942635f88c4fa99791efd6c
Brandon Ferguson <syntaxic@gmail.com>
2018-06-30 14:47:43 -0400

    Added Event Model

---
+++ EventApp/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventApp.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string Guests { get; set; }
+        public string Description { get; set; }
+    }
+}
