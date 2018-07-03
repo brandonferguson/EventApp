30a88837e1eba4be73bf82453953e4c441b297ec
Brandon Ferguson <syntaxic@gmail.com>
2018-07-03 18:04:50 -0400

    Fixed initialized data

--- EventApp/DAL/EventInit.cs
+++ EventApp/DAL/EventInit.cs
@@ -13,7 +13,7 @@ namespace EventApp.DAL
         {
             var events = new List<Event>
             {
-                new Event{EventName="Beer Brew Day!",Date=DateTime.Parse("2018-01-20"),Location="Joe's House",Description="Come drink and brew beer with us!",Guests="Joe, Michael, Wes, Rose"},
+                new Event{EventName="Beer Brew Day",Date=DateTime.Parse("2018-01-20"),Location="Joe's House",Description="Come drink and brew beer with us!",Guests="Joe, Michael, Wes, Rose"},
                 new Event{EventName="Josh's Birthday Dinner",Date=DateTime.Parse("2018-02-07"),Location="Selina's Tavern",Description="Josh is older and needs to eat food to celebrate",Guests="Josh, Dunca, Joe, Michael, Wes, Rose"},
                 new Event{EventName="Boardgame Day",Date=DateTime.Parse("2018-01-07"),Location="Joe's House",Description="We'll be playing boardgames and drinking",Guests="Guy, Joe, Wes, Rose"}
             };
